b289058608796d0a8d84ce5611e0913db787eb1a
Jesse Leoanrd <jesse1517@gmail.com>
2019-07-18 16:46:12 -0700

    the rest of the todolist

---
+++ src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Learn React",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Smile",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Clean my Room",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Play Guitar",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "go to grocery store",
+        completed: false
+    }
+]
+
+export default todosData
